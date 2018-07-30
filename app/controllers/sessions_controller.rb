89ece1be81fa79ace672b29368808e9aa3d78dec
vagrant <vagrant@myapp.(none)>
2018-07-31 00:09:26 +0900

    login logout with sorecy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,20 @@
 class SessionsController < ApplicationController
-
   def new
   end
 
   def create
+    if user = login(params[:email], params[:password])
+      flash[:notice] = 'login success'
+      redirect_to root_path
+    else
+      flash[:notice] = 'email or password incorrect'
+      redirect_to new_session_path
+    end
+  end
+
+  def destroy
+    logout
+    flash[:notice] = 'logout success'
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,21 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  validates :email, presence: true, uniqueness: true
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/,
+              message: "email format error" }
   validates :password, presence: true,
     confirmation: true,
     length: { minimum: 6 },
     if: :need_with_validate_password
   validates :password_confirmation, presence: true,
     if: :need_with_validate_password
+
+  def username
+    self.email.split('@').first
+  end
 
   private
 
